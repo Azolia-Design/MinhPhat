729aba0063b51dd0248b51fa44a5fa36b96cf176
Azolia-Design <58262885+Azolia-Design@users.noreply.github.com>
2020-07-01 15:30:12 +0700

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -8,15 +8,6 @@ document.querySelector(".menu_toggle_close").addEventListener("click", () => {
     document.querySelector(".blackout").style.display = "none";
 });
 
-function myFunction() {
-  var x = document.getElementById("myDIV");
-  if (x.style.display === "none") {
-    x.style.display = "block";
-  } else {
-    x.style.display = "none";
-  }
-}
-
 $(window).scroll(function() {    
   var scroll = $(window).scrollTop();
   //console.log(scroll);
